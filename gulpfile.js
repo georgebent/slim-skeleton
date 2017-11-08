f7c77d195b98b142f8a63527241db6fd319c1eab
George <gdevyur@gmail.com>
2017-11-08 20:31:24 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('sass', function() {
         .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
         .pipe(rename({suffix: '.min', prefix : ''}))
         .pipe(autoprefixer(['last 15 versions']))
-        .pipe(cleanCSS()) // Опционально, закомментировать при отладке
+        .pipe(cleanCSS())
         .pipe(gulp.dest('tmp/public/css'))
         .pipe(browserSync.reload({stream: true}));
 });
