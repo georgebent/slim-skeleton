0e152e00b82ec6cdabe8340010bbf0b89e5039a2
Vania Kucher <dev.kucher@gmail.com>
2017-11-09 19:44:25 +0200

    Update settings.php

--- src/settings.php
+++ src/settings.php
@@ -1,12 +1,18 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        
+        // set to false in production
+        'displayErrorDetails' => true,
+        
+         // Allow the web server to send the content-length header
+        'addContentLengthHeader' => false,
+        
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../public/',
         ],
+        
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
